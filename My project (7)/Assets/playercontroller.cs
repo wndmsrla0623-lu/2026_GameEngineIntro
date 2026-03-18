aab328f05e2a143a94d7e6b6505aaa7581bfee4b
LSH005 <isihyeon005shingu@gmail.com>
2026-03-18 16:18:39 +0900

    플레이어 이동 및 점프

--- My project (7)/Assets/playercontroller.cs
+++ My project (7)/Assets/playercontroller.cs
@@ -5,13 +5,31 @@ public class playercontroller : MonoBehaviour
 {
     private Vector2 moveInput;
     public float moveSpeed = 7f;
+    public float jumpForce = 7f;
+    private Rigidbody2D rb;
+    private Animator myAnimator;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myAnimator.SetBool("move", false);
+    }
     
     public void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
     }
 
-    // Update is called once per frame
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed) //점프 버튼을 누르면
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+  
     void Update()
     {
         if (moveInput.x > 0)
@@ -21,6 +39,15 @@ public class playercontroller : MonoBehaviour
         else if (moveInput.x < 0)
         {
             transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if(moveInput.magnitude > 0)
+        {
+            myAnimator.SetBool("move", true);
+        }
+        else
+        {
+            myAnimator.SetBool("move", false);
         }
         transform.Translate(Vector3.right * moveSpeed * moveInput.x * Time.deltaTime);
     }
